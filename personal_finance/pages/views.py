cc61251307af7d9527bd7f429ef87d97637b2417
A1vot <Alvot2026@gmail.com>
2026-05-06 15:20:50 +0900

    Создан простой шаблон ЛК и настроен маршрут

--- personal_finance/pages/views.py
+++ personal_finance/pages/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'pages/login.html')
+
+def dashboard_view(request):
+    return render(request, 'pages/dashboard.html')
